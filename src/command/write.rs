50c80758e55ff1b0260c83e251cb2c5e71926eba
Juan Hernández <juan.hernandez.babon@gmail.com>
2016-04-24 21:22:41 +0200

    Very basic write command.
    * It uses the file file ~/.slate.json.
    * It writes new values.
    * It updates existing values.

--- src/command/write.rs
+++ src/command/write.rs
@@ -1,3 +1,8 @@
+use std::env;
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::HashMap;
+use rustc_serialize::json;
 use docopt::Docopt;
 use utils;
 
@@ -12,10 +17,12 @@ If <value> is not present, <stdin> will be used.
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
-    arg_key: Vec<String>,
-    arg_value: Vec<String>,
+    arg_key: String,
+    arg_value: String,
 }
 
+// TODO: Return Result so the main program can show messages
+// and errors
 pub fn run(argv: Vec<String>) {
     let args: Args = Docopt::new(USAGE)
                             .and_then(|d| d.argv(argv)
@@ -23,5 +30,34 @@ pub fn run(argv: Vec<String>) {
                                            .decode())
                             .unwrap_or_else(|e| e.exit());
 
-    println!("Args: {:?}", args);
+    let mut path = match env::home_dir() {
+        Some(home) => home,
+        None => panic!("No HOME dir found"),
+    };
+    path.push(".slate.json");
+
+    // read current state
+    let mut r = match File::open(&path) {
+        Ok(file) => file,
+        Err(_) => panic!("Cannot open file"),
+    };
+
+    let mut buffer = String::new();
+    r.read_to_string(&mut buffer);
+
+    let mut slate: HashMap<String, String> = json::decode(&buffer).unwrap();
+
+    // write new values
+    slate.insert(args.arg_key, args.arg_value);
+
+    let encoded = json::encode(&slate).unwrap();
+
+    let mut f = match File::create(&path) {
+        Ok(file) => file,
+        Err(_) => panic!("Cannot create file"),
+    };
+    match f.write_all(encoded.as_bytes()) {
+        Ok(_) => { println!("OK") },
+        Err(_) => { panic!("Couldn't save file") }
+    };
 }
